c9bc3373cbc6cec3b2e4c5a2086503d2c149e630
Sivateja <ajjarapusivateja123@gmail.com>
2026-01-02 16:20:58 +0530

    Day-01 assignment

--- day-01/system_health.py
+++ day-01/system_health.py
@@ -9,4 +9,28 @@ def check_cpu_threshold():
         print("CPU  EMAIL ALERT SENT....")
     else:
         print("CPU is in  safe state")
+ 
+
+def check_virtualmemory():
+    memory_threshold=int(input("Enter the memory threshold"))
+    current_memory=psutil.virtual_memory().percent
+    print("current memory is: ",current_memory)
+    if(current_memory>memory_threshold):
+        print("memory exceeded! ")
+    else:
+        print("Memory is available to use")
+
+
+def check_disk():
+    disk_threshold=int(input("Enter the disk threshold"))
+    current_disk=psutil.disk_usage("/").percent
+    print("current disk is: ",current_disk)
+    if(current_disk>disk_threshold):
+        print("disk exceeded! ")
+    else:
+        print("disk is available to use")
+
+
 check_cpu_threshold() 
+check_virtualmemory()
+check_disk()       
